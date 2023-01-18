1dd7ae9d9d07415312407487314a94621f744816
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2023-01-18 23:13:40 +0530

    post call

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const fs = require("fs/promises");
 const express = require("express");
-const cors = require('cors');
-const _ =  require("lodash");
-const {v4:uuid} = require("uuid");
+const cors = require("cors");
+const _ = require("lodash");
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/data", (req,res) =>{
-    const colors = ["Black", "Red", "Pink", "Orange"];
-    const shapes = ["Circle", "Triangle", "Square", "Rectangle"];
-    
-    res.json({
-        color: _.sample(colors),
-        shape: _.sample(shapes)
-    })
-})
+app.get("/data", (req, res) => {
+  const colors = ["Black", "Red", "Pink", "Orange"];
+  const shapes = ["Circle", "Triangle", "Square", "Rectangle"];
 
-app.listen(300,() => console.log('running'));
+  res.json({
+    color: _.sample(colors),
+    shape: _.sample(shapes),
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const id = uuid();
+  const content = req.body.content;
+
+  if (!content) {
+    return res.status(400);
+  }
+
+  await fs.mkdir("data/files", { recursive: true });
+
+  res.sendStatus(201);
+});
+
+app.listen(300, () => console.log("running"));
