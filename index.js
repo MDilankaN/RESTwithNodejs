7de5de54dfdc39c9bebea1fb9469e8dfa06d361c
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2023-01-18 23:01:50 +0530

    get method

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ const {v4:uuid} = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/data", (req,res) =>{
+    const colors = ["Black", "Red", "Pink", "Orange"];
+    const shapes = ["Circle", "Triangle", "Square", "Rectangle"];
+    
+    res.json({
+        color: _.sample(colors),
+        shape: _.sample(shapes)
+    })
+})
+
 app.listen(300,() => console.log('running'));
